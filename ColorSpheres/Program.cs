8171218faa64805a42f74be103a32efa09a5c98f
André <imandreleandro@gmail.com>
2025-04-03 23:30:43 +0100

    fixed spheres

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -8,9 +8,29 @@ namespace ColorSpheres
         {
             Console.WriteLine("Hello LP1!");
 
-            Color color = new Color(100, 100, 200);
+            Color color = new Color(80, 70, 200);
+            Color color2 = new Color(200, 80, 70);
+
+            Sphere sphere1 = new Sphere(color, 5);
+            Sphere sphere2 = new Sphere(color2, 8);
+            Console.WriteLine($"Sphere1: {sphere1.GetTimesThrown()}");
+            Console.WriteLine($"Sphere2: {sphere2.GetTimesThrown()}");
             
-            Console.WriteLine($"Grey: {color.GetGrey()}");
+            sphere1.Throw();
+            sphere2.Throw();
+            sphere2.Throw();
+            sphere2.Throw();
+            
+            Console.WriteLine($"Sphere1: {sphere1.GetTimesThrown()}");
+            Console.WriteLine($"Sphere2: {sphere2.GetTimesThrown()}");
+            
+            sphere2.Pop();
+            sphere2.Throw();
+            sphere2.Throw();
+            sphere2.Throw();
+            
+            Console.WriteLine($"Sphere1: {sphere1.GetTimesThrown()}");
+            Console.WriteLine($"Sphere2: {sphere2.GetTimesThrown()}");
         }
     }
 }
