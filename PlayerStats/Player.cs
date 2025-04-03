af0aa0f706cfbfdc9af98a3cd79412f910a37514
André <imandreleandro@gmail.com>
2025-04-04 00:35:40 +0100

    Added Player class and project PlayerStats

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,43 @@
+﻿namespace PlayerStats
+{
+    public class Player
+    {
+        private float highScore;
+        private int playedGames;
+        private int wonGames;
+        
+        public float HighScore
+        {
+            get => highScore;
+            set
+            {
+                if (value > highScore) highScore = value;
+            }
+        }
+        
+        public string Name { get; }
+        public float WinRate
+        {
+            get
+            {
+                if (playedGames == 0) return 0;
+                return ((float) wonGames) / playedGames;
+            }
+        }
+        
+        public void PlayGame(bool win)
+        {
+            playedGames++;
+            if (win) wonGames++;
+        }
+        
+        public Player(string name)
+        {
+            Name = name;
+            highScore = 0;
+            playedGames = 0;
+            wonGames = 0;
+        }
+        
+    }
+}
