9457d5db5df6ef6b5250e3dc9ef64208400f624f
André <imandreleandro@gmail.com>
2025-04-04 14:09:54 +0100

    commit

---
+++ MyRPG/Player.cs
@@ -0,0 +1,40 @@
+﻿namespace MyRPG
+{
+    public class Player
+    {
+
+        private int xp;
+        private float health;
+        
+        // change xp if bigger.
+        public int XP
+        {
+            get => xp;
+            set
+            {
+                if (value > xp) xp = value;
+            }
+        }
+        
+        public int Level
+        {
+            get => 1 + (xp / 1000);
+        }
+
+        public float MaxHealth
+        {
+            get => 100f + (Level - 1) * 20f;
+        }
+
+        public Player(string name)
+        {
+            
+        }
+
+        public void TakeDamage(float damage)
+        {
+            
+        }
+
+    }
+}
